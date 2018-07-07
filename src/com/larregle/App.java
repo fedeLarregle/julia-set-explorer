7d10cb17d314667d4fc819b418af041610754465
DESKTOP-VMSGSAC\fedel <fede.larregle@gmail.com>
2018-07-07 17:43:33 -0300

    Adding JFrame initializer.

--- src/com/larregle/App.java
+++ src/com/larregle/App.java
@@ -1,7 +1,20 @@
 package com.larregle;
 
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
 public class App {
     public static void main(String... args) {
-
+        EventQueue.invokeLater(()-> {
+            JFrame f = new JFrame();
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f.setDefaultLookAndFeelDecorated(true);
+            f.setResizable(false);
+            JuliaExplorer je = new JuliaExplorer();
+            f.add(je, BorderLayout.CENTER);
+            f.pack();
+            f.setVisible(true);
+        });
     }
 }
